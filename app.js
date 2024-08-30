ed14da9556def5a0446261cf96897e1bbfa86883
arthur-user <115193136+arthur-user@users.noreply.github.com>
2024-08-29 23:27:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
-app.get("health", (req, res) => {
+//health check endpoint
+app.get("/health", (req, res) => {
   res.sendStatus(200);
 });
 
